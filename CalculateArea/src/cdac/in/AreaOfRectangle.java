1a5525958e5d14dc968501bc9b51bb7ecac35e2e
Ashok_Patil <developer.ashok@gmail.com>
2023-05-27 02:08:33 +0530

    Modified as directed of SOP

--- CalculateArea/src/cdac/in/AreaOfRectangle.java
+++ CalculateArea/src/cdac/in/AreaOfRectangle.java
@@ -5,8 +5,10 @@ public class AreaOfRectangle {
 	public static void main(String[] args) {
     int length = 5;  
     int width = 2;  
-    int area = length * width;  
+    int area = length * width;
+    int perimeter = (length*2) + (width*2);
     System.out.println("Area of rectangle: " + area);
+    System.out.println("perimeter of rectangle: " + perimeter);
 
 	}
 
